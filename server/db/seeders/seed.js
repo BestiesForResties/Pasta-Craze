8d1d00db90aa4f8bfac570b68638b69020078317
miavh123 <miahug123@gmail.com>
2023-09-09 14:15:13 -0400

    added footer updated seed data as well

--- server/db/seeders/seed.js
+++ server/db/seeders/seed.js
@@ -1,57 +1,74 @@
-const { User, Item, Cart, sequelize } = require('../index');
+const { User, Item, CartItem, Cart, sequelize } = require('../index');
 
 const seed = async () => {
+    try {
+        await sequelize.sync({ force: true }); // Drop and recreate tables
+    
+        console.log("Table has been dropped and recreated");
 
-sequelize.sync({force:true})
-  .then(async () => {
-  await User.create(
-    { username: 'admin',
-      email: 'admin@email.com',
-      password: 'test',
-      is_Admin: 1
-  });
 
-  await User.create(
-    { username: 'damian',
-      email: 'damian@email.com',
-      password: '1234',
-      is_Admin: 0
-  });
 
-  await User.create(
-    { username: 'mia',
-      email: 'mia@email.com',
-      password: '1234',
-      is_Admin: 0
-  });
+        await User.bulkCreate([
+            { username: 'damianulloa',
+              email: 'damianulloa@email.com',
+              password: 'damian',
+              is_Admin: 0,
+            },
+            { username: 'miahuggins',
+              email: 'miahuggins@email.com',
+              password: 'mia',
+              is_Admin: 0,
+            },
+            { username: 'admin',
+              email: 'admin@email.com',
+              password: 'test',
+              is_Admin: 1
+            },
+        ]);
+    
+        await Item.bulkCreate([
+          {
+            id: 1,
+            name: 'Chicken Carbonara',
+            description:'creamy chicken carbonara is inspired by a classic Italian (Roman) pasta dish made with bacon or pancetta, whisked egg, and hard cheese.',
+            price: 12.99,
+            category: 'Pasta',
+            image_url: 'https://hips.hearstapps.com/hmg-prod/images/hdm-chickencorbonara-14753-1564436398.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=1200:*,'
+          },
+          {
+            id: 2,
+            name: 'Fettuccine Alfredo',
+            description: 'Creamy Alfredo sauce Fettuccine pasta with a homemade decadent Alfredo sauce made with 5 simple ingredients parmesan cheese and butter.',
+            price: 11.99,
+            category: 'Pasta',
+            image_url: 'https://amindfullmom.com/wp-content/uploads/2016/01/Light-Fettucine-Alfredo-Recipe.jpg',
+          },
+          {
+            id: 3,
+            name: 'Bake Ziti',
+            description: 'A casserole with ziti pasta and a Neapolitan-style tomato sauce.',
+            price: 10.99,
+            category: 'Pasta',
+            image_url: 'https://www.thecountrycook.net/wp-content/uploads/2020/02/EASY-BAKED-ZITI-thumbnail.jpg',
+          },
+          {
+            id: 4,
+            name: 'Lasagna',
+            description: 'Made of very wide, flat sheets. Either term can also refer to an Italian dish made of stacked layers of lasagna alternating with fillings such as ragù, béchamel sauce, vegetables, cheeses, and seasonings and spices..',
+            category: 'Pasta',
+            image_url: 'https://houseofnasheats.com/wp-content/uploads/2020/09/classic-lasagna-recipe-22.jpg',
+    
+          },
+    
+           
+        ]);
 
-  await Item.bulkCreate([
-      {name: 'Penne',
-      type: 'noodle',
-      description: 'Penne is an extruded type of pasta with cylinder-shaped pieces, their ends cut at an angle. Penne is the plural form of the Italian penna, deriving from Latin penna, and is a cognate of the English word pen.',
-      price: 1.50},
-      {name: 'Rigatoni',
-      type: 'noodle',
-      description: 'Rigatoni is a type of pasta originating in Italy. They are larger than penne and ziti, and sometimes slightly curved. They are not as curved as elbow macaroni.',
-      price: 1.99},
-      {name: 'Marinara',
-      type: 'Sauce',
-      description: 'A simple sauce blended from tomatoes and aromatic ingredients such as onions and garlic. Additional seasonings and herbs such as basil, oregano, red pepper flakes, and more can be added depending on personal preference.',
-      price: 3.15},
-      {name: 'Alfredo',
-      type: 'Sauce',
-      description: 'The sauce consists of heavy cream or half and half that is mixed with butter, grated Parmesan cheese, pepper, and occasionally nutmeg to create a rich Italian meal.',
-      price: 3.99},
-      {name: 'ground beef',
-      type: 'toppings',
-      description: 'Lean ground beef to make your pasta hearty and healthy',
-      price: 1.75},
-      {name: 'ground sausage',
-      type: 'toppings',
-      description: 'Lean ground sausage to give your pasta great flavor',
-      price: 1.99},
-  ]);
-  })
+        console.log("Data has been inserted into tables");
+    } catch (error) {
+        console.error('Error syncing tables:', error);
+    } finally {
+        sequelize.close();
+    }
   .catch((error) => {
     console.error('Error syncing models:', error);
   });
