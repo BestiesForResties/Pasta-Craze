78154405a7901b869aefda68c535aca9c79dd1b2
miavh123 <116587742+miavh123@users.noreply.github.com>
2023-09-09 20:23:19 -0400

    navbar (#4)
    * navbar
    
    * Adding git ignores to client and server and updating ReadME (#6)
    
    Co-authored-by: Damesdev <damian.ulloa@clover.com>
    
    * navbar
    
    * Removing Node_modules from commit
    
    ---------
    
    Co-authored-by: Damesdev <104599832+Damesdev@users.noreply.github.com>
    Co-authored-by: Damesdev <damian.ulloa@clover.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Navbar from './NavBar.js'
+import styles from './Navbar.module.css'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <header className={styles.header}>
+        <h1 className={styles.heading}>Pasta Craze</h1>
+        <p className={styles.description}></p>
+      </header>
+      <main>
+        
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+      
+        {/* The rest of your content */}
+      </main>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById('root'));
