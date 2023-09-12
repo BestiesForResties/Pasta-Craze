b6569b78ae7681df5d664e7a708d19af8601b6c6
miavh123 <miahug123@gmail.com>
2023-09-12 13:38:42 -0400

    footer adjusted

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,43 +5,35 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-       
-      </div>
-      <div>
-        <h2> Contact 
-          <br /> 
-          
-        </h2>
-      </div>
-      <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            WHERE PASTA MEETS ADDICTING 
+            WHERE PASTA MEETS ADDICTING
           </h2>
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
           <p className={styles.text}>
-            8374 PARK AVE 
+            8374 PARK AVE
             <br />Bronx New York, 10458
             <br /> (718) 378-2929
           </p>
-       
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>WORKING HOURS</h1>
           <p className={styles.text}>
             MONDAY - FRIDAY
             <br /> 12:00AM – 11:00PM
           </p>
-          <p className= {styles.text}>
-            
-            SATURDAY - SUNDAY 
-           <br /> 12:00PM - 1:00AM
-            
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00PM - 1:00AM
           </p>
-       
         </div>
+      </div>
+      <div className={styles.contact}>
+        <h2>Contact</h2>
+        <p className={styles.text}>
+          PastaCraze@info.com 
+          <br />(718) 378-2929
+        </p>
       </div>
     </div>
   );
