87edcb5f41c72d01f39206a564145a1f1de9ca35
miavh123 <miahug123@gmail.com>
2023-09-13 12:39:22 -0400

    home page added

---
+++ client/src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Contact = () => {
+    // Define your seed dat
+  
+<p> mfjgmdj </p>
+}
+export default Contact;
+
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Home.module.css'; // Make sure to import your styles
-import backgroundImage from './background.webp'; // Adjust the path to your background image
+import styles from './Home.module.css';
+import backgroundImage from './background.webp';
 
 const Home = () => {
   return (
-    <div className={styles['home-container']}>
-      <div className={styles['background-image']} style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className={styles.header}>
-          <div className={styles.topHeader}>
-            <h1 className={styles.largeHeader}>Pasta Craze</h1>
-            <Link to="/menu">
-              <button className={styles.orderButton}>Order Now</button>
-            </Link>
-          </div>
-          <h2 className={styles.smallHeader}>Where Pasta Meets Addicting</h2>
+    <div
+      className={styles['home-container']}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className={styles.header}>
+        <div className={styles.topHeader}>
+          <h1 className={styles.largeHeader}>
+            Welcome to<span className={styles.craze}> Pasta Craze</span>
+          </h1>
+          <p className={styles.catchphrase}>
+            Explore the flavors<span className={styles.highlight}> beyond pasta</span>
+          </p>
+          <Link to="/menu">
+            <button className={styles.orderButton}>Order Now</button>
+          </Link>
         </div>
       </div>
     </div>
