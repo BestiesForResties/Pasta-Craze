a063d5545a4a9dfddd927a3fc2466ac08fa9ab2a
Damesdev <damian.ulloa@clover.com>
2023-09-28 14:23:04 -0400

    changing .env location to root

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes} = require('sequelize');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
